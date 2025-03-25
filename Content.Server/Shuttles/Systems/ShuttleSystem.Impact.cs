ThetaStation/ThetaStation

Новый таран (#468)
* Update ShuttleSystem.Impact.cs

* also modifier stuff while I'm at it

Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
@@ -1,4 +1,3 @@
-using Content.Server.Explosion.EntitySystems;
 using System.Numerics;
 using Content.Server.Shuttles.Components;
 using Robust.Server.GameObjects;
@@ -6,24 +5,31 @@ using Content.Shared.Audio;
 using Robust.Shared.Audio;
 using Robust.Shared.Map;
 using Robust.Shared.Physics.Events;
-using Robust.Shared.Player;
 using Content.Shared.Theta.ShipEvent.Components;
 using Robust.Shared.Map.Components;
-using Content.Shared.Roles.Theta;
+using Content.Shared.Damage;
 
 namespace Content.Server.Shuttles.Systems;
 
 public sealed partial class ShuttleSystem
 {
-    [Dependency] private readonly TransformSystem _formSys = default!;
-    [Dependency] private readonly ExplosionSystem _expSys = default!;
+    [Dependency] private readonly MapSystem _mapSys = default!;
+    [Dependency] private readonly DamageableSystem _damageSys = default!;
 
     /// <summary>
     /// Minimum velocity difference between 2 bodies for a shuttle "impact" to occur.
     /// </summary>
     private const int MinimumImpactVelocity = 10;
 
-    private const double IntensityMultiplier = 0.01; //carefully picked by trial & error
+    /// <summary>
+    /// Kinetic energy required to dismantle a single tile
+    /// </summary>
+    private const float TileBreakEnergy = 5000;
+
+    /// <summary>
+    /// Kinetic energy required to spawn sparks
+    /// </summary>
+    private const float SparkEnergy = 7000;
 
     private readonly SoundCollectionSpecifier _shuttleImpactSound = new("ShuttleImpactSound");
 
@@ -34,7 +40,8 @@ public sealed partial class ShuttleSystem
 
     private void OnShuttleCollide(EntityUid uid, ShuttleComponent component, ref StartCollideEvent args)
     {
-        if (!HasComp<MapGridComponent>(args.OtherEntity))
+        if (!TryComp<MapGridComponent>(uid, out var ourGrid) ||
+            !TryComp<MapGridComponent>(args.OtherEntity, out var otherGrid))
             return;
 
         if (TryComp<ShipEventTeamMarkerComponent>(uid, out var ourMark) &&
@@ -65,14 +72,36 @@ public sealed partial class ShuttleSystem
         if (jungleDiff < MinimumImpactVelocity)
             return;
 
+        var energy = ourBody.Mass * Math.Pow(jungleDiff, 2) / 2;
+        var dir = (ourVelocity.Length() > otherVelocity.Length() ? ourVelocity : -otherVelocity).Normalized();
+        ProcessTile(uid, ourGrid, (Vector2i) ourPoint, (float) energy, -dir);
+        ProcessTile(args.OtherEntity, otherGrid, (Vector2i) otherPoint, (float) energy, dir);
+
         var coordinates = new EntityCoordinates(ourXform.MapUid.Value, args.WorldPoint);
         var volume = MathF.Min(10f, 1f * MathF.Pow(jungleDiff, 0.5f) - 5f);
         var audioParams = AudioParams.Default.WithVariation(SharedContentAudioSystem.DefaultVariation).WithVolume(volume);
-
         _audio.PlayPvs(_shuttleImpactSound, coordinates, audioParams);
+    }
 
-        var kineticEnergy = ourBody.Mass * Math.Pow(jungleDiff, 2) / 2;
-        var intensity = (float) (kineticEnergy * IntensityMultiplier);
-        _expSys.QueueExplosion(coordinates.ToMap(EntityManager, _formSys), ExplosionSystem.DefaultExplosionPrototypeId, intensity, 5f, 50f);
+    private void ProcessTile(EntityUid uid, MapGridComponent grid, Vector2i tile, float energy, Vector2 dir)
+    {
+        DamageSpecifier damage = new();
+        damage.DamageDict = new() { { "Blunt", energy } };
+
+        foreach (EntityUid localUid in _lookup.GetLocalEntitiesIntersecting(uid, tile, gridComp: grid))
+        {
+            _damageSys.TryChangeDamage(localUid, damage);
+
+            TransformComponent form = Transform(localUid);
+            if (!form.Anchored)
+                _transform.Unanchor(localUid, form);
+            _throwing.TryThrow(localUid, dir);
+        }
+
+        if (energy > TileBreakEnergy)
+            _mapSys.SetTile(new Entity<MapGridComponent>(uid, grid), tile, Tile.Empty);
+
+        if (energy > SparkEnergy)
+            SpawnAtPosition("EffectSparks", new EntityCoordinates(uid, tile));
     }
 }

trest100/space-station-14

Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
@@ -6,15 +6,33 @@ using Robust.Shared.Map;
 using Robust.Shared.Physics.Dynamics;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Player;
+using Robust.Shared.Map.Components;
+using Content.Shared.Damage;
+using Robust.Server.GameObjects;
+
 
 namespace Content.Server.Shuttles.Systems;
 
 public sealed partial class ShuttleSystem
 {
+    [Dependency] private readonly MapSystem _mapSys = default!;
+    [Dependency] private readonly DamageableSystem _damageSys = default!;
+
+
     /// <summary>
     /// Minimum velocity difference between 2 bodies for a shuttle "impact" to occur.
     /// </summary>
     private const int MinimumImpactVelocity = 10;
+
+    /// <summary>
+    /// Kinetic energy required to dismantle a single tile
+    /// </summary>
+    private const float TileBreakEnergy = 5000;
+
+    /// <summary>
+    /// Kinetic energy required to spawn sparks
+    /// </summary>
+    private const float SparkEnergy = 7000;
 
     private readonly SoundCollectionSpecifier _shuttleImpactSound = new("ShuttleImpactSound");
 
@@ -25,7 +43,10 @@ public sealed partial class ShuttleSystem
 
     private void OnShuttleCollide(EntityUid uid, ShuttleComponent component, ref StartCollideEvent args)
     {
-        if (!HasComp<ShuttleComponent>(args.OtherEntity))
+
+        if (!TryComp<ShuttleComponent>(uid, out var ourGrid) ||
+            !TryComp<ShuttleComponent>(args.OtherEntity, out var otherGrid))
+
             return;
 
         var ourBody = args.OurBody;
@@ -51,10 +72,37 @@ public sealed partial class ShuttleSystem
             return;
         }
 
+        var energy = ourBody.Mass * Math.Pow(jungleDiff, 2) / 2;
+        var dir = (ourVelocity.Length() > otherVelocity.Length() ? ourVelocity : -otherVelocity).Normalized();
+        ProcessTile(uid, ourGrid, (Vector2i) ourPoint, (float) energy, -dir);
+        ProcessTile(args.OtherEntity, otherGrid, (Vector2i) otherPoint, (float) energy, dir);
+
         var coordinates = new EntityCoordinates(ourXform.MapUid.Value, args.WorldPoint);
         var volume = MathF.Min(10f, 1f * MathF.Pow(jungleDiff, 0.5f) - 5f);
         var audioParams = AudioParams.Default.WithVariation(SharedContentAudioSystem.DefaultVariation).WithVolume(volume);
-
         _audio.PlayPvs(_shuttleImpactSound, coordinates, audioParams);
+    }
+
+    private void ProcessTile(EntityUid uid, MapGridComponent grid, Vector2i tile, float energy, Vector2 dir)
+    {
+        DamageSpecifier damage = new();
+        damage.DamageDict = new() { { "Blunt", energy } };
+
+        foreach (EntityUid localUid in _lookup.GetLocalEntitiesIntersecting(uid, tile, gridComp: grid))
+        {
+            _damageSys.TryChangeDamage(localUid, damage);
+
+            TransformComponent form = Transform(localUid);
+            if (!form.Anchored)
+                _transform.Unanchor(localUid, form);
+            _throwing.TryThrow(localUid, dir);
+        }
+
+        if (energy > TileBreakEnergy)
+            _mapSys.SetTile(new Entity<MapGridComponent>(uid, grid), tile, Tile.Empty);
+
+        if (energy > SparkEnergy)
+            SpawnAtPosition("EffectSparks", new EntityCoordinates(uid, tile));
+
     }
 }